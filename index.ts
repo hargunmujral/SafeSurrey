51bfca91acebfeee9db4d3ba7db8f96a46883401
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-05-01 13:50:33 -0400

    got a help command and  slash command perms working

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import DiscordJS, { Intents } from 'discord.js';
+import DiscordJS, { Intents, Interaction } from 'discord.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -11,7 +11,35 @@ const client = new DiscordJS.Client({
 
 client.on('ready', () => {
     console.log('Ready!');
+
+    const guildID = '929889176097275924'
+    const guild = client.guilds.cache.get(guildID)
+    let commands 
+        if(guild){
+            commands = guild.commands
+        } else {
+            commands = client.application?.commands
+        }
+    commands?.create({
+        name: 'help',
+        description: 'Displays all commands',
+    })
 });
+
+ client.on('interactionCreate', async (interaction) => {
+     if(!interaction.isCommand()) {
+         return;
+     }
+
+    const {commandName, options} = interaction
+    
+        if(commandName === 'help') {
+            interaction.reply({
+                content: 'Here is the function of the bot.',
+                ephemeral: true,
+            })
+        }
+    })
 
 client.on('messageCreate', (message) => {
     if(message.content === 'test') {
